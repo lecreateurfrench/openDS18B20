42660086b0531c38e8ddc62542a1db6f05960c49
lecreateurfrench <jeremy.venin@gmail.com>
2016-09-26 20:30:43 +0200

    setup working and erase function

--- open_ds18b20/__main__.py
+++ open_ds18b20/__main__.py
@@ -1,21 +1,26 @@
 import sys
+import os
 import re
 import getpass
 import time
 import subprocess
 import file, mail, probe
 
-try: 
-	config = file.ConfigFile("./config.json")
-except IOError:
-	print "creation du fichier config.json"
-	subprocess.Popen(["touch", "config.json"])
-	time.sleep(1) #leaves enough time for the subprocess to create the file
-	config = file.ConfigFile("./config.json")
 
-files = []
-settings = {"email":"","password":"","done":False}
-prompt = '> '
+def initialConfig():
+	try: 
+		CONFIG = file.ConfigFile("./config.json")
+	except IOError:
+		print "creation du fichier config.json"
+		subprocess.Popen(["touch", "config.json"])
+		time.sleep(1) #leaves enough time for the subprocess to create the file
+		CONFIG = file.ConfigFile("./config.json")
+	return CONFIG
+
+FILES = []
+SETTINGS = {"email":"","password":"","done":False}
+PROMPT = '> '
+CONFIG = initialConfig()
 
 def writeDependencies(file):
 	print "before continuing you should add \"w1-gpio\" and \"w1-therm\" to /etc/modules files"
@@ -23,12 +28,12 @@ def writeDependencies(file):
 
 def promptConfig():
 	print("adress where emails are going to be send and sent from ? ")
-	settings["email"] = raw_input(prompt)
+	SETTINGS["email"] = raw_input(PROMPT)
 	print("password ? (warning the password will be kept clear in the config file)")
-	settings["password"]= getpass.getpass()
-	settings["done"]=True
-	config.register(settings)
-	return
+	SETTINGS["password"]= getpass.getpass()
+	SETTINGS["done"]=True
+	CONFIG.register(SETTINGS)
+	return 
 
 def modulesTester():
 	flag = [False, False]
@@ -48,33 +53,35 @@ def main():
 	tester = modulesTester()
 	if tester == False:
 		return
-	if config.nbline > 0: 
-		config.readData()
-		if config.is_done() == False:
+	if len(sys.argv) > 1:
+		if str(sys.argv[1]) == "erase":
+			os.remove("./config.json")
+			initialConfig()
 			promptConfig()
-	else:
+			CONFIG.readData()
+	elif CONFIG.nbline ==  0:
 		promptConfig()
-		config.readData()
+	CONFIG.readData()
 	probes = probe.Probe()
 	probes.detectProbe()
 	try: 
 		for p in range(len(probes.listprobes)):
-			files.append(file.ProbeFile(probes.listprobes[p]))
-			templine = files[p].readLine(2)
+			FILES.append(file.ProbeFile(probes.listprobes[p]))
+			templine = FILES[p].readLine(2)
 			probes.getTemperature(templine)
 	except:
 		print "temperatures couldn't be read : ", sys.exc_info()[0]
 	try:
 		email = mail.Mail()
 		email.messageBody(probes.temperatures)
-		email.credentials["email"], email.credentials["password"] = config.getCredentials()
+		email.credentials["email"], email.credentials["password"] = CONFIG.getCredentials()
 		email.messageBuilder(email.credentials["email"], email.credentials["email"], "list of temperatures")
 		email.sendMail()
 	except:
 		print "mail couldn't be send : ", sys.exc_info()[0]
-	for i in range(len(files)):
-		files[i].closeFile()
-	config.closeFile()
+	for i in range(len(FILES)):
+		FILES[i].closeFile()
+	CONFIG.closeFile()
 	return
 
 if __name__ == '__main__':
